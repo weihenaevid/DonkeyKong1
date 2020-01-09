e31d6914bdb75d76177fe194bfaf71a76aae7026
imanasif <34377100+imanasif@users.noreply.github.com>
2020-01-09 14:45:11 -0500

    level2

---
+++ ladder1.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ladder1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ladder1 extends Actor
+{
+    
+    /**
+     * Act - do whatever the ladder1 wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //GreenfootImage ladder1 = new GreenfootImage("ladder.png");
+        //ladder1.scale(50,80);
+        //getladder1().drawImage(ladder1, 400,200);
+        // Add your action code here.
+    }    
+}
